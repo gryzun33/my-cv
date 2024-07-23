8a8fec2b24d6970e3451bb5f487376ed9a2433f5
gryzun33 <yakushevao777@gmail.com>
2024-07-23 17:42:05 +0300

    feat: add fonts

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
-import './index.css';
-import { CssBaseline } from '@mui/material';
+import './index.scss';
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <CssBaseline />
     <App />
   </React.StrictMode>,
 );
