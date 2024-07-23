647c4a2ca0fd3439670eb87e19bbf39fccc0865b
gryzun33 <yakushevao777@gmail.com>
2024-07-23 19:41:32 +0300

    feat: add plugin vite-tsconfig-paths

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,28 @@
 import './App.scss';
+import Description from 'components/Description/Description';
+import Contacts from 'components/Contacts/Contacts';
+import Skills from 'components/Skills/Skills';
+import Education from 'components/Education/Education';
+import Experience from 'components/Experience/Experience';
+import EnglishLevel from 'components/EnglishLevel/EnglishLevel';
 
 function App(): JSX.Element {
   return (
     <>
-      <div className="container">vjkhk</div>
+      <div className="container">
+        <Description />
+        <div className="info">
+          <div className="info-left">
+            <Contacts />
+            <Skills />
+          </div>
+          <div className="info-right">
+            <Education />
+            <Experience />
+            <EnglishLevel />
+          </div>
+        </div>
+      </div>
     </>
   );
 }
