af31b65760c1394535d96d3031e206912ab4a547
gryzun33 <yakushevao777@gmail.com>
2024-07-24 15:53:38 +0300

    feat: add skills

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -1,7 +1,27 @@
 import './Skills.scss';
 
 function Skills(): JSX.Element {
-  return <div></div>;
+  return (
+    <section className="skills">
+      <h2>Skills</h2>
+      <ul>
+        <li>JavaScript</li>
+        <li>TypeScript</li>
+        <li>
+          React
+          <br />
+          (Zustand, TanStack Query, React Router, React Hook Form)
+        </li>
+
+        <li>HTML/CSS</li>
+        <li>SASS</li>
+        <li>Git, Github</li>
+        <li>Vite, Webpack</li>
+        <li>Material UI</li>
+        <li>Figma</li>
+      </ul>
+    </section>
+  );
 }
 
 export default Skills;
--- src/components/TopSection/TopSection.tsx
+++
@@ -1,7 +0,0 @@
-import './TopSection.scss';
-
-function TopSection(): JSX.Element {
-  return <div></div>;
-}
-
-export default TopSection;
