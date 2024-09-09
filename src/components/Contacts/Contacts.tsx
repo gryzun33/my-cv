7905c50013f27124aedc08b43b74644067e0a932
gryzun33 <yakushevao777@gmail.com>
2024-09-09 13:45:55 +0300

    feat: chnage link

--- src/components/Contacts/Contacts.tsx
+++ src/components/Contacts/Contacts.tsx
@@ -34,7 +34,7 @@ function Contacts(): JSX.Element {
           </a>
         </li>
         <li>
-          <a href="yakushevaolga564@gmail.com" target="_blank">
+          <a href="mailto:yakushevaolga564@gmail.com" target="_blank">
             <img src={iconMail} alt="mail" width="20" height="20" />
             <span>yakushevaolga564@gmail.com</span>
           </a>
